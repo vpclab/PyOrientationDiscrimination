36973b26154d9c46e4c162ddfac27e7b6f349bc1
Dominic Canare <dom@greenlightgo.org>
2018-06-09 00:23:09 -0500

    Move dependencies into setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='OrientationDiscrimination',
+	version='1.0',
+	description='Orientation discrimination threshold estimation using Gabor patches and Best PEST',
+	author='Dominic Canare',
+	author_email='dom@greenlightgo.org',
+	url='http://greenlightgo.org',
+	packages=['BestPest'],
+	install_requires=[
+		'psychopy==1.90.1'
+	],
+)
